359cdcdca5d70bbeee71ebdc87e447ce77d5822b
christabel888 <christabelmohammed1@gmail.com>
2022-10-14 04:48:01 -0700

    1CTask3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet programming
+ *
+ * Return: 0
+ */
+int main(void)
+i{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
