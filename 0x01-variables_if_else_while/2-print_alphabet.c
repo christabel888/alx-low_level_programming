f1497e99eb55a22c50f55049fd3608ff326f6e63
christabel888 <christabelmohammed1@gmail.com>
2022-10-14 04:26:45 -0700

    1CTask2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative or alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
