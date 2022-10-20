26388821fe087edc77298bda2d02d1ce1d1adbc2
Christabel Mohammed <105973473+christabel888@users.noreply.github.com>
2022-10-21 00:10:31 +0200

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * main - Prints out the larget prime factor of 612852475143
+ * Return:return 0 with success.
  */
-void print_number(int n)
+int main(void)
 {
-unsigned int n1;
+	long i;
+	long num = 612852475143;
 
-if (n < 0)
-{
-n1 = -n;
-_putchar('-');
-}
-else
-{
-n1 = n;
-}
-
-if (n1 / 10)
-{
-print_number(n1 / 10);
-}
-
-_putchar((n1 % 10) + '0');
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
 }
