f4ae2471d4263fa81f3ef34b084bb92049193e26
christabel888 <christabelmohammed1@gmail.com>
2022-10-20 12:28:49 -0700

    4Ctask4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = 0; c <= 9; c++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar(i + '0');
+		if (!(c == 2 || c == 4))
+			_putcha(c);
 	}
 	_putchar('\n');
 }
