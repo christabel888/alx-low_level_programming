19bd487f286c78f8267a8ccce1b8a36e7e5ecd54
christabel888 <christabelmohammed1@gmail.com>
2022-11-04 06:39:37 -0700

    bigctask

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ *
+ *   Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	/*(void) agrv;*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
